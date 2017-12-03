0782ecab817f35e8983a881abace1af3591e50b1
Henrique Sugiyama <hsugiyama@ymail.com>
2017-12-03 20:04:43 -0200

    correcoes popular combos

--- src/Telas/Vagao/AdicionarVagao.java
+++ src/Telas/Vagao/AdicionarVagao.java
@@ -7,9 +7,11 @@ import javax.swing.*;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
+import Entidades.Locomotiva;
 import Entidades.Vagao;
 import Entidades.VeiculoFerroviario;
 import Repositorio.Factory;
+import Repositorio.FactoryLayout;
 import Repositorio.Controller;
 
 import Telas.Interface.ITelas;
@@ -54,10 +56,43 @@ public class AdicionarVagao extends JFrame{
 	private JComboBox<Vagao.Tipo> JCBTipo;
 	
 	//declarando seções da janela
-	JPanel Jhead;
 	JPanel Jbody;
 	JPanel Jfooter;
 	
+	//declarando factory para chamada de telas.
+	FactoryLayout tela;
+	//JPanel Jprincipal;
+	
+	//declarando factory para chamada de telas.
+	private FactoryLayout fLayout = new FactoryLayout();
+	
+	//construtor para que seja visualizado pelo usuario na tela da composição(readonly)
+	public AdicionarVagao(Vagao v) {
+		this();
+		vagao = v;
+
+		//textfield
+		this.JTComprimento.setText(String.valueOf(vagao.getComprimento()));
+		this.JTProprietario.setText(String.valueOf(vagao.getProprietario()));
+		
+		//comboBox		
+		this.JCBBitola.setSelectedItem(VeiculoFerroviario.Bitola.valueOf(String.valueOf(vagao.getBitola())));
+		this.JCBTipo.setSelectedItem(Vagao.Tipo.valueOf(String.valueOf(vagao.getTipo())));
+		this.JCBSubTipo.setSelectedItem(Vagao.SubTipo.valueOf(String.valueOf(vagao.getSubTipo())));
+		
+		//deixando readonly
+		this.JTComprimento.setEnabled(false);		
+		this.JTProprietario.setEnabled(false);
+		this.JCBBitola.setEnabled(false);
+		this.JCBTipo.setEnabled(false);
+		this.JCBSubTipo.setEnabled(false);
+		JBExcluir.setVisible(false);
+		JBNovo.setVisible(false);
+		JBSalvar.setVisible(false);
+		
+		//setando o titulo
+		this.setTitle(v.getIdentificacao());
+	}
 	
 	//em caso tenha vagao adicionado, o botão excluir ira aparecer na tela
 	//Setando campos do vagao para caso haja adicionado, ira poder excluir.
@@ -70,18 +105,32 @@ public class AdicionarVagao extends JFrame{
 		this.JTComprimento.setText(String.valueOf(vagao.getComprimento()));
 		this.JTProprietario.setText(String.valueOf(vagao.getProprietario()));
 		
-		//comboBox
+		this.JTProprietario.setEnabled(false);
+		
+		//comboBox		
 		this.JCBBitola.setSelectedItem(VeiculoFerroviario.Bitola.valueOf(String.valueOf(vagao.getBitola())));
 		this.JCBTipo.setSelectedItem(Vagao.Tipo.valueOf(String.valueOf(vagao.getTipo())));
 		this.JCBSubTipo.setSelectedItem(Vagao.SubTipo.valueOf(String.valueOf(vagao.getSubTipo())));
 		
+		this.JCBBitola.setEnabled(false);
+		this.JCBTipo.setEnabled(false);
+		this.JCBSubTipo.setEnabled(false);
+		
 		//botão
 		JBExcluir.setVisible(true); 
+		
+		//setando o titulo da janela
+		this.setTitle("Alterar Vagão");
+	}
+	
+	public AdicionarVagao(VagaoTableModel md) {
+		this(); // ajusta título
+		this.setTitle("Adicionar Vagao");
 	}
 	
 	//metodo para intanciar meus componentes e seta as secoes na janela
-	public AdicionarVagao(VagaoTableModel md) {
-		modelo = md;
+	public AdicionarVagao() {
+		super();
 		
 		//campos de texto
 		JTComprimento = new JTextField();
@@ -120,32 +169,16 @@ public class AdicionarVagao extends JFrame{
 		setLocation(450, 300);
 		setResizable(false);
 		
-		//Jhead();
 		Jbody();
 		Jfooter();	
 		
 		Container cp = getContentPane();
 		cp.setLayout(new BorderLayout());
-		//cp.add(Jhead, BorderLayout.NORTH);
 		cp.add(Jbody, BorderLayout.CENTER);
 		cp.add(Jfooter, BorderLayout.SOUTH);
 		pack();
+		tela = new FactoryLayout();
 	}
-	
-	public AdicionarVagao() {
-		// TODO Auto-generated constructor stub
-	}
-
-	//titulo da janela
-	/*private void Jhead() { 
-		FormLayout layouthead = new FormLayout(
-				"pref, 5dlu, 50dlu, 20dlu, min,pref, 5dlu, 50dlu, 5dlu, min", // colunas
-				"pref, 2dlu, pref, 2dlu, pref"); // linha
-		Jhead = new JPanel(layouthead);
-		CellConstraints cc = new CellConstraints();
-		//titulo
-		Jhead.add(JLTitulo, cc.xy(1, 1));
-	}*/
 	
 	//conteudo do formulário
 	private void Jbody() { 
@@ -154,30 +187,27 @@ public class AdicionarVagao extends JFrame{
 				"pref, 2dlu, pref, 2dlu, pref"); // linha
 		Jbody = new JPanel(layouthead);
 		CellConstraints cc = new CellConstraints();
-		//bitola
-		Jbody.add(JCBBitola, cc.xy(1, 1));
-		JCBBitola = new JComboBox<VeiculoFerroviario.Bitola>();
-		Jbody.add(JCBBitola, cc.xy(3, 1));
+		//proprietario
+		Jbody.add(JLProprietario, cc.xy(1, 1));
+		JTProprietario= new JTextField();
+		Jbody.add(JTProprietario, cc.xy(3, 1));
 		
 		//comprimento
 		Jbody.add(JLComprimento, cc.xy(5, 1));
 		JTComprimento = new JTextField();
 		Jbody.add(JTComprimento, cc.xy(8,1));
 		
-		//proprietario
-		Jbody.add(JLProprietario, cc.xy(1, 3));
-		JTProprietario= new JTextField();
-		Jbody.add(JTProprietario, cc.xyw(3,3,5));
+		//Tipo
+		Jbody.add(JLTipo, cc.xy(1, 3));
+		Jbody.add(JCBTipo, cc.xy(3,3));
 		
 		//Subtipo
-		Jbody.add(JCBSubTipo, cc.xy(1, 3));
-		JCBSubTipo= new JComboBox<Vagao.SubTipo>();
-		Jbody.add(JCBSubTipo, cc.xyw(3,3,5));
+		Jbody.add(JLSubtipo, cc.xy(5, 3));
+		Jbody.add(JCBSubTipo, cc.xy(8,3));
 		
-		//Tipo
-		Jbody.add(JCBTipo, cc.xy(1, 3));
-		JCBTipo= new JComboBox<Vagao.Tipo>();
-		Jbody.add(JCBTipo, cc.xyw(3,3,5));
+		//bitola
+		Jbody.add(JLBitola, cc.xy(1, 5));
+		Jbody.add(JCBBitola, cc.xy(3, 5));
 	}
 	
 	//botoes do formulario
@@ -216,51 +246,49 @@ public class AdicionarVagao extends JFrame{
 			try {
 				valorComprimento = Double.parseDouble(JTComprimento.getText());
 			}catch(NumberFormatException err ){
-				JOptionPane.showMessageDialog(null,"O campo comprimento necessita ser preenchido corretamente!");
+				fLayout.openAlertError(null,"O campo comprimento necessita ser preenchido corretamente!");
 				return;
 			}
-			String valorProprietario = "";
-			//proprietario
-			try {
-				valorProprietario = JTProprietario.getText();
-				if(valorProprietario.isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Por favor, preecha o valor do proprietário.");
-				} else if (valorProprietario.length() != 6) {
-					JOptionPane.showMessageDialog(null, "Por favor, preecha o valor do proprietário corretamente.");
-					return;
-				}
-			} catch (Exception e2) {
-				JOptionPane.showMessageDialog(null,"O campo proprietário necessita ser preenchido corretamente!");
-				return;
-			}
+			String valorProprietario = JTProprietario.getText();
 			
 			//comboBox
-			String valorBitola = JCBBitola.getSelectedItem().toString();
-			String valorSubTipo = JCBSubTipo.getSelectedItem().toString();
-			String valorTipo = JCBTipo.getSelectedItem().toString(); 
+			VeiculoFerroviario.Bitola valorBitola = (Entidades.VeiculoFerroviario.Bitola) JCBBitola.getSelectedItem();
+			Vagao.SubTipo valorSubtipo = (Entidades.Vagao.SubTipo) JCBSubTipo.getSelectedItem();
+			Vagao.Tipo valorTipo = (Entidades.Vagao.Tipo) JCBTipo.getSelectedItem();
 			
+			if(valorProprietario.isEmpty()) {
+				fLayout.openAlertError(null,"Preencha a propritário, o vgão não pode ser salvo sem preencher!");
+				return;
+			} else if(JCBBitola.getSelectedIndex() == -1) {
+				fLayout.openAlertError("Bitola","A Bitola necessita ser preenchido corretamente.");
+				return;
+			} else if(JCBSubTipo.getSelectedIndex() == -1) {
+				fLayout.openAlertError("Subtipo","O Subtipo necessita ser preenchido corretamente.");
+			} else if(JCBTipo.getSelectedIndex() == -1) {
+				fLayout.openAlertError("Tipo","O Tipo necessita ser preenchudo corretamente.");
+			}
 			
 			//acao para caso clique no botao salvar e todos os campos estejam certos
 			//conexao com o banco de dados, salvar no banco de dados e desconectar do banco
 			Factory f = new Factory();
 			Controller c = f.getController();
 			try {
-				Vagao v = f.getVagao(VeiculoFerroviario.Bitola.A, Vagao.Tipo.A, Vagao.SubTipo.A, valorProprietario.toCharArray(), valorComprimento);
+				Vagao v = f.getVagao(valorBitola, valorTipo, valorSubtipo, valorProprietario.toCharArray(), valorComprimento);
 				//Vagao v = f.getVagao(valorBitola.charAt(0), valorTipo.charAt(0), valorSubTipo.charAt(0), valorProprietario.toCharArray(), valorComprimento);
 				c.connect();
 				if(vagao == null){
 					//c.create(v);//salvar no banco
-					JOptionPane.showMessageDialog(null,"Vagão salvo com sucesso!");
+					fLayout.openAlertWarning(null,"Vagão salvo com sucesso!");
 					//modelo.addVagao(v);
 				}
 				else {
 					//c.update(v);
-					JOptionPane.showMessageDialog(null,"Vagãos alterado com sucesso!");
+					fLayout.openAlertWarning(null,"Vagãos alterado com sucesso!");
 					//modelo.updateVagao(linha, v);
 				}
 				
 			}catch(Exception err){
-				JOptionPane.showMessageDialog(null,err.getMessage());	
+				fLayout.openAlertWarning(null,err.getMessage());	
 			}finally{
 				c.disconnect();//desconectar do banco
 			}
@@ -272,11 +300,11 @@ public class AdicionarVagao extends JFrame{
 	//ação botao Novo
 	ActionListener jbNovoActLt = new ActionListener() {
 		  public void actionPerformed(ActionEvent e) {
-				JTBitola.setText(" ");
 				JTComprimento.setText(" ");
 				JTProprietario.setText(" ");
-				JTSubtipo.setText(" ");
-				JTTipo.setText(" ");
+				JCBBitola.setSelectedItem(null);
+				JCBSubTipo.setSelectedItem(null);
+				JCBTipo.setSelectedItem(null);
 		}
 	};
 	
@@ -285,18 +313,21 @@ public class AdicionarVagao extends JFrame{
 		public void actionPerformed(ActionEvent e) {
 			Factory f = new Factory();
 			Controller c = f.getController();
-			try {
-				c.connect();
-				c.remove(vagao);
-				JOptionPane.showMessageDialog(null,"O vagão foi removido com sucesso!");
-				modelo.removeVagao(linha);
-			}catch(Exception err){
-				JOptionPane.showMessageDialog(null, err.getMessage());
-				return;
-			} finally{
-				c.disconnect();
+			int confirm = fLayout.openConfirm("Tem certeza que deseja excluir o vagão?");
+			if(confirm == 0) {
+				try {
+					c.connect();
+					c.remove(vagao);
+					fLayout.openAlertInfo(null,"O vagão foi removido com sucesso!");
+					modelo.removeVagao(linha);
+				}catch(Exception err){
+					fLayout.openAlertError(null, err.getMessage());
+					return;
+				} finally{
+					c.disconnect();
+				}
+				dispose();	
 			}
-			dispose();
 		}
 	};
 	
